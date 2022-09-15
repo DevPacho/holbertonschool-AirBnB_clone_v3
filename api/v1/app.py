737532639a5d57285e01cb299d7a8182326f8bdd
DevPacho <frankjose2004@gmail.com>
2022-09-15 18:20:20 -0500

    We did the task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """ Starting the API! """
 
 from os import getenv
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -19,6 +19,11 @@ app.register_blueprint(app_views)
 def close(self):
     """Removes the current SQLAlchemy Session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
