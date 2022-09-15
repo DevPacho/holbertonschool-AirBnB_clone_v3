e235434d9ce8deaf00ad5cf4d03e713cda1dcb99
DevPacho <frankjose2004@gmail.com>
2022-09-15 18:04:31 -0500

    We did the task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,23 @@ from flask import jsonify
 def status():
     """Defines the request status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False, methods=["GET"])
+def stats():
+    """Retrieves the number of each objects by type"""
+
+    from models import storage
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)})
