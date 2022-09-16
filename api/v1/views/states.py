d960fceb8096786deeea16ad92d7eab892505d84
DevPacho <frankjose2004@gmail.com>
2022-09-16 00:09:32 -0500

    First approach to task 7

---
+++ api/v1/views/states.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+""" Handles all default RESTFul API actions """
+
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models.state import State
+from models import storage
+from models.base_model import BaseModel
+from console import HBNBCommand
+
+
+@app_views.route("/states", strict_slashes=False, methods=["GET"])
+def get_states():
+    """Retrieves the list of all State objects"""
+
+    all_states = []
+    for state in list(storage.all(State).values()):
+        all_states.append(state.to_dict())
+    return jsonify(all_states)
+
+
+@app_views.route("/states/<state_id>", strict_slashes=False, methods=["GET"])
+def get_state_by_id(state_id):
+    """Retrieves a State object"""
+
+    for state in list(storage.all(State).values()):
+        if state.id == state_id:
+            return jsonify(state.to_dict())
+
+    abort(404)
+
+
+@app_views.route(
+    "/states/<state_id>", strict_slashes=False, methods=["DELETE"])
+def delete_state_by_id(state_id):
+    """Deletes a State object"""
+
+    for state in list(storage.all(State).values()):
+        if state.id == state_id:
+            object = storage.get(State, state_id)
+            storage.delete(object)
+            storage.save()
+            return jsonify({}), 200
+
+    abort(404)
+
+
+@app_views.route("/states", strict_slashes=False, methods=["POST"])
+def post_states():
+    """Creating a new state"""
+
+    try:
+        if "name" not in request.get_json().keys():
+            return jsonify("Missing name"), 400, {'Content-Type':
+                                                  'application/json'}
+
+        new_state = State(**request.get_json())
+        new_state.save()
+
+        return jsonify(new_state.to_dict()), 201
+
+    except Exception:
+        return jsonify("Not a JSON"), 400, {'Content-Type':
+                                            'application/json'}
